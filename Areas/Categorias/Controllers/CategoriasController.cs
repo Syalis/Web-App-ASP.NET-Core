f57036f46a19de9e0eddfc659557e409e9f1edfe
Borja Montes <bmontesgz@gmail.com>
2019-12-16 15:45:59 +0100

    Update: CategoriasController

--- Areas/Categorias/Controllers/CategoriasController.cs
+++ Areas/Categorias/Controllers/CategoriasController.cs
@@ -2,16 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ProyectoWeb.Areas.Categorias.Models;
+using ProyectoWeb.Controllers;
+using ProyectoWeb.Library;
+using ProyectoWeb.Models;
+using SistemasWeb.Data;
 
 namespace ProyectoWeb.Areas.Categorias.Controllers
 {
     [Area("Categorias")]
     public class CategoriasController : Controller
     {
+        private TCategoria _categoria;
+        private LCategorias _lcategorias;
+        private SignInManager<IdentityUser> _signInManager;
+        private static DataPaginador<TCategoria> models;
+
+        public CategoriasController(ApplicationDbContext context, SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+            _lcategorias = new LCategorias(context);
+        }
+
+
         public IActionResult Categoria()
         {
-            return View();
+            if (_signInManager.IsSignedIn(User))
+            {
+                models = new DataPaginador<TCategoria>
+                {
+                    Input = new TCategoria()
+                };
+                return View(models);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+
         }
     }
+
 }
---
+++ Library/LCategorias.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SistemasWeb.Data;
+
+namespace ProyectoWeb.Library
+{
+    public class LCategorias
+    {
+        private ApplicationDbContext context;
+
+        public LCategorias(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+    }
+}
